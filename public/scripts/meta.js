4456cca6b2645df9a272fb967830bd7fdeabd9d2
Haruka <96925398+nakoyasha@users.noreply.github.com>
2024-04-14 22:06:21 +0300

    chore: Move the views folder out of the server folder feat: Import bootstrap, make the builds menu a bit more stylish, and make the build details page somewhat functional

--- public/scripts/meta.js
+++ public/scripts/meta.js
@@ -1,27 +1,31 @@
 function closeWarning() {
-    const warning = document.getElementById("wip-feature")
+  const warning = document.getElementById("wip-feature");
 
-    warning.remove()
+  warning.remove();
 }
 
 function toggleDiv(divId, buttonId) {
-    const div = document.getElementById(divId)
-    const button = document.getElementById(buttonId)
-    const isVisible = div.style.display == "flex"
+  const div = document.getElementById(divId);
+  const button = document.getElementById(buttonId);
+  const isVisible = div.style.display == "flex";
 
-    if (isVisible != true) {
-        div.style.display = "flex"
-        button.innerText = "Hide"
-    } else {
-        div.style.display = "none"
-        button.innerText = "Show"
-    }
+  if (isVisible != true) {
+    div.style.display = "flex";
+    button.innerText = "Hide";
+  } else {
+    div.style.display = "none";
+    button.innerText = "Show";
+  }
 }
 
 function toggleStrings(buttonId) {
-    toggleDiv("strings-diff", buttonId)
+  toggleDiv("strings-diff", buttonId);
 }
 
 function toggleExperiments(buttonId) {
-    toggleDiv("experiments-diff", buttonId)
+  toggleDiv("experiments-diff", buttonId);
 }
+
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip();
+});
