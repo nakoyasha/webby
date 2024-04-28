a037f5656ad6a5022ebe8bc0017387a92ae0ab1f
Haruka <96925398+nakoyasha@users.noreply.github.com>
2024-04-28 20:55:22 +0300

    chore: make astro builds work

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,18 @@
 import { defineConfig } from "astro/config";
-
 import react from "@astrojs/react";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
   output: "server",
+  adapter: node({
+    mode: "standalone",
+  }),
+  vite: {
+    ssr: {
+      noExternal: ["path-to-regexp"],
+    },
+  },
 });
