dd5c066dda29f6cbe82b1f754c5f4a4fd55dfa44
Haruka <96925398+nakoyasha@users.noreply.github.com>
2024-05-02 05:38:11 +0300

    fix: Incorrect time displays (e.g 1 days ago)

--- src/client/src/components/Discord/BuildCard.tsx
+++ src/client/src/components/Discord/BuildCard.tsx
@@ -11,17 +11,17 @@ function timeDifference(current: number, previous: number) {
   const elapsed = current - previous;
 
   if (elapsed < msPerMinute) {
-    return Math.round(elapsed / 1000) + " seconds ago";
+    return Math.round(elapsed / 1000) + " second(s) ago";
   } else if (elapsed < msPerHour) {
-    return Math.round(elapsed / msPerMinute) + " minutes ago";
+    return Math.round(elapsed / msPerMinute) + " minute(s) ago";
   } else if (elapsed < msPerDay) {
-    return Math.round(elapsed / msPerHour) + " hours ago";
+    return Math.round(elapsed / msPerHour) + " hour(s) ago";
   } else if (elapsed < msPerMonth) {
-    return Math.round(elapsed / msPerDay) + " days ago";
+    return Math.round(elapsed / msPerDay) + " day(s) ago";
   } else if (elapsed < msPerYear) {
-    return Math.round(elapsed / msPerMonth) + " months ago";
+    return Math.round(elapsed / msPerMonth) + " month(s) ago";
   } else {
-    return Math.round(elapsed / msPerYear) + " years ago";
+    return Math.round(elapsed / msPerYear) + " year(s) ago";
   }
 }
 
@@ -46,7 +46,7 @@ export default class BuildCard extends React.Component {
     this.setState({
       timeElapsed: timeDifference(
         new Date().getTime(),
-        this.props.buildData.date_found.getTime()
+        this.props.buildData.built_on.getTime()
       ),
     });
   }
@@ -70,15 +70,6 @@ export default class BuildCard extends React.Component {
             this.props.onClick(this.props.buildData);
           }}
         >
-          {/* <div className="build-indicators">
-            <div
-              data-toggle="tooltip"
-              data-placement="top"
-              title={`This is a ${buildData.branches[0]} build`}
-              className={`build-indicator build-indicator-${buildData.branches[0]}`}
-            ></div>
-          </div> */}
-
           <div className="build-card">
             <h3>
               <b>Build {buildData.build_number}</b>
